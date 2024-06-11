b126842f157bb93e8a3a3d8deb27e0afc494c818
Neha Bijoy <nehabijoy@vt.edu>
2024-06-10 22:10:35 -0400

    Initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+
+import "./About.css";
+
+import profileImage from "../assets/profile_pic_name.jpg";
+
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img 
+              className="profile_image"
+              // C:\Verizon\NehaTest.jpg
+              // the image is imported
+              src = {profileImage}
+              alt="Profile Pic"
+              ></img>
+            </div>
+            </div>
+             <div className="split right"> 
+              <div className="centered">
+              <div className="name_title">Neha Bijoy</div>
+              <div className="brief_description">
+              I'm Neha Bijoy!
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     )
   }
